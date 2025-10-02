859824b7ce0d5cc1b37a8e4909d0e29f0e322223
AM-MUGAMBI <amugambi370@gmail.com>
2025-10-02 22:30:44 +0300

    redo

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,5 @@
 # listings/urls.py
 
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ListingViewSet, BookingViewSet
 
@@ -8,6 +7,4 @@ router = DefaultRouter()
 router.register(r'listings', ListingViewSet)
 router.register(r'bookings', BookingViewSet)
 
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
+urlpatterns = router.urls
